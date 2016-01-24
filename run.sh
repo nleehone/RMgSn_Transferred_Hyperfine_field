0c99adf50e69e6d9f435f8ae6a71cb97f05a8fa5
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2016-01-24 10:54:32 -0800

    Add TbMgSn cycloidal calculation
    The run.py had a typo which made it not generate the plot.

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ python3 RMgSn_Transferred_Bhf_plot.py results/DyMgSn_14p8K.dat plots/DyMgSn_14p8
 python3 RMgSn_Transferred_Bhf.py config/TbMgSn_sine_4K.py results/TbMgSn_sine_4K.dat
 python3 RMgSn_Transferred_Bhf_plot.py results/TbMgSn_sine_4K.dat plots/TbMgSn_sine_4K.png
 python3 RMgSn_Transferred_Bhf.py config/TbMgSn_cycloidal_4K.py results/TbMgSn_cycloidal_4K.dat
-python3 RMgSn_Transferred_Bhf_plot.py results/TMbgSn_cycloidal_4K.dat plots/TbMgSn_cycloidal_4K.png
+python3 RMgSn_Transferred_Bhf_plot.py results/TbMgSn_cycloidal_4K.dat plots/TbMgSn_cycloidal_4K.png 0.1
 
 # ErMgSn
 python3 RMgSn_Transferred_Bhf.py config/ErMgSn_k0p8_4K.py results/ErMgSn_k0p8_4K.dat
